222db0073153aa08cafd221a0bd16b00e565d983
Ivan Zinovyev <ivan.zinovyev@touchin.ru>
2016-12-19 18:23:17 +0300

    Remove redundant properties

--- UIAnimatedTextField/Source/String+Extensions.swift
+++ UIAnimatedTextField/Source/String+Extensions.swift
@@ -10,14 +10,6 @@ import Foundation
 
 extension String {
     
-    var localized: String {
-        return NSLocalizedString(self, comment: "")
-    }
-    
-    var isMultiline: Bool {
-        return range(of: "\n") != nil
-    }
-    
     var isValidEmail: Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
         let emailPredicat = NSPredicate(format: "SELF MATCHES %@", emailRegex)
